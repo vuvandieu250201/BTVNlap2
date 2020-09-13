a346e97ddc82a482f067be51b425c2bf30f60fec
vuvandieu250201 <71130456+vuvandieu250201@users.noreply.github.com>
2020-09-13 11:11:07 +0700

    BTVNlap02VuVanDieu

---
+++ bai3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main3(void){
+	int a,b,tong,hieu,tich,thuong;
+	printf("nhap a = ", a);
+	scanf("%d", &a);
+	printf("nhap b = ", b);
+	scanf("%d", &b);
+	tong=a+b;
+	printf("tong a va b = %d\n ", tong);
+	hieu=a-b;
+	printf("hieu a va b = %d\n", hieu);
+	tich=a*b;
+	printf("tich a va b = %d\n", tich);
+	thuong=a/b;
+	printf("thuong a va b = %d\n", thuong);
+	return 0;
+	
+	
+}
